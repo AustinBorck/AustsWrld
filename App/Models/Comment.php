6f86b464c2c4fa54f61ee45f9cb861320e65f488
aust <aeb157@yahoo.com>
2023-06-11 15:37:07 -0600

    stub out models for posts and comments

--- App/Models/Comment.php
+++ App/Models/Comment.php
@@ -5,4 +5,46 @@ require('../db.php');
 
 class Comment {
     public static $table = 'comments';
+    public $id;
+    public $post_id;
+    public $user_id;
+    public $body;
+    public $date_created;
+    public $active;
+
+    public function __construct($id, $post_id, $user_id, $body, $date_created, $active) {
+        $this->id = $id;
+        $this->post_id = $post_id;
+        $this->user_id = $user_id;
+        $this->body = $body;
+        $this->date_created = $date_created;
+        $this->active = $active;
+    }
+
+    public function mapData($data) {
+        $this->id = $data['id'];
+        $this->post_id = $data['post_id'];
+        $this->user_id = $data['user_id'];
+        $this->body = $data['body'];
+        $this->date_created = $data['date_created'];
+        $this->active = $data['active'];
+    }
+
+    public function loadFromDB()
+    {
+        require('../db.php');
+        $stmt = $pdo->prepare("SELECT * FROM comments WHERE id = :id");
+        $stmt->bindParam(':id', $this->id);
+        $stmt->execute();
+    }
+
+    public function getPostComments($post_id)
+    {
+        require('../db.php');
+        $stmt = $pdo->prepare("SELECT * FROM comments WHERE post_id = :post_id AND active = 1 ORDER BY date_created DESC");
+        $stmt->bindParam(':post_id', $post_id);
+        $stmt->execute();
+        $comments = $stmt->fetchAll();
+        return $comments;
+    }
 }
--- App/Models/Post.php
+++ App/Models/Post.php
@@ -5,4 +5,53 @@ require('../db.php');
 
 class Post {
     public static $table = 'posts';
+
+    public $id;
+    public $user_id;
+    public $title;
+    public $body;
+    public $author;
+    public $date_created;
+    public $date_updated;
+    public $active;
+
+    public function __construct($id, $user_id, $title, $body, $author, $date_created, $date_updated, $active) {
+        $this->id = $id;
+        $this->user_id = $user_id;
+        $this->title = $title;
+        $this->body = $body;
+        $this->author = $author;
+        $this->date_created = $date_created;
+        $this->date_updated = $date_updated;
+        $this->active = $active;
+    }
+
+    public function mapData($data) {
+        $this->id = $data['id'];
+        $this->user_id = $data['user_id'];
+        $this->title = $data['title'];
+        $this->body = $data['body'];
+        $this->author = $data['author'];
+        $this->date_created = $data['date_created'];
+        $this->date_updated = $data['date_updated'];
+        $this->active = $data['active'];
+    }
+
+    public function loadFromDB()
+    {
+        require('../db.php');
+        $stmt = $pdo->prepare("SELECT * FROM posts WHERE id = :id");
+        $stmt->bindParam(':id', $this->id);
+        $stmt->execute();
+    }
+
+    public function getAllPosts()
+    {
+        require('../db.php');
+        $stmt = $pdo->prepare("SELECT * FROM posts WHERE active = 1 ORDER BY date_created DESC");
+        $stmt->execute();
+        $posts = $stmt->fetchAll();
+        return $posts;
+    }
+
 }
