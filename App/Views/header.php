34a60caf5c13c2558059f500b1e822754ac994ea
aust <aeb157@yahoo.com>
2023-06-11 13:38:34 -0600

    added files for messages and changed layout of the nav bar

--- App/Views/header.php
+++ App/Views/header.php
@@ -1,12 +1,14 @@
 <header>
     <div class="navbar">
-        <div class="icon">
-            <a href="dashboard.php"><img src="../../App/svgs/logo.svg" alt="SVG Image"></a>
+        <div class="left-links">
+            <div class="icon">
+                <a href="dashboard.php"><img src="../../App/svgs/logo.svg" alt="SVG Image"></a>
+            </div>
+            <a href="profile.php">Profile</a>
+            <a href="messages.php">Messages</a>
         </div>
-        <a href="profile.php">Profile</a>
         <form action="/../App/Controllers/logout.php" method="post">
-            <button type="submit" name="logout" value="1">Logout</button>
+            <button type="submit" name="logout" class="logout-button" value="1">Logout</button>
         </form>
     </div>
 </header>
-<!-- App\Controllers -->
